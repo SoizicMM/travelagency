b63840c685169a2f21555a5e1cd293c49ab70115
Meety004 <115738016+Meety004@users.noreply.github.com>
2024-02-29 19:03:09 +0000

    Route logout

--- main.py
+++ main.py
@@ -7,17 +7,25 @@ app = Flask("TravelAgency")
 def index():
   return render_template("index.html")
 
+
 @app.route('/login')
 def login():
   return render_template("login.html")
+
+@app.route('/logout')
+def logout():
+  return render_template("index.html")
+
 
 @app.route('/register')
 def register():
   return render_template("register.html")
 
+
 @app.route('/continent')
 def continent():
   return render_template("continent.html")
+
 
 @app.route('/destination')
 def destination():
